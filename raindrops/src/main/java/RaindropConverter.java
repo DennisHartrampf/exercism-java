839589515d18cd8c86ade32fc6ad2c9c3c60a9b2
dennis.hartrampf <dennis.hartrampf@postcon.de>
2018-11-27 12:55:01 +0100

    Raindrops

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,21 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder result = new StringBuilder();
+        if (isDivisibleBy(number, 3)) {
+            result.append("Pling");
+        }
+        if (isDivisibleBy(number, 5)) {
+            result.append("Plang");
+        }
+        if (isDivisibleBy(number, 7)) {
+            result.append("Plong");
+        }
+        return result.length() > 0 ? result.toString() : String.valueOf(number);
+    }
+
+    private boolean isDivisibleBy(int number, int divisor) {
+        return number % divisor == 0;
     }
 
 }
