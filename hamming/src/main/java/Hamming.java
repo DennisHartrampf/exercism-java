532ccae4d74166c0d30c6f96f5fc21b83c815812
dennis.hartrampf <dennis.hartrampf@postcon.de>
2018-12-03 12:10:42 +0100

    Applied feedback on Hamming Distance

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -3,18 +3,20 @@ class Hamming {
     private final int hammingDistance;
 
     Hamming(final String leftStrand, final String rightStrand) {
-        if (leftStrand.length() != rightStrand.length()) {
-            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
-        }
+        verifyThatBothStrandsHaveSameLength(leftStrand, rightStrand);
         this.hammingDistance = calculateHammingDistance(leftStrand, rightStrand);
     }
 
+    private void verifyThatBothStrandsHaveSameLength(String leftStrand, String rightStrand) {
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+    }
+
     private int calculateHammingDistance(String leftStrand, String rightStrand) {
-        final var leftChars = leftStrand.toCharArray();
-        final var rightChars = rightStrand.toCharArray();
         var hammingDistanceCounter = 0;
         for (int i = 0; i < leftStrand.length(); i++) {
-            if (leftChars[i] != rightChars[i]) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
                 hammingDistanceCounter++;
             }
         }
