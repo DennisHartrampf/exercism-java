d8e43bef0637ac37c38b4a9be94642d4551f585c
dennis.hartrampf <dennis.hartrampf@postcon.de>
2019-01-14 14:19:31 +0100

    Rotational cipher (Caesar cipher)

---
+++ rotational-cipher/src/main/java/RotationalCipher.java
@@ -0,0 +1,33 @@
+class RotationalCipher {
+
+    private static final int LOWER_A = 'a';
+    private static final int UPPER_A = 'A';
+    private static final int NUMBER_OF_LETTERS = 26;
+
+    private final int shiftKey;
+
+    RotationalCipher(int shiftKey) {
+        this.shiftKey = shiftKey % 26;
+    }
+
+    String rotate(String data) {
+        return data
+                   .chars()
+                   .mapToObj(this::encodeChar)
+                   .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                   .toString();
+    }
+
+    private char encodeChar(int aChar) {
+        return (char) (Character.isAlphabetic(aChar) ? shiftChar(aChar) : aChar);
+    }
+
+    private int shiftChar(int aChar) {
+        return Character.isLowerCase(aChar) ? shiftChar(aChar, LOWER_A) : shiftChar(aChar, UPPER_A);
+    }
+
+    private int shiftChar(int aChar, int intOfFirstLetter) {
+        return (aChar + shiftKey - intOfFirstLetter) % NUMBER_OF_LETTERS + intOfFirstLetter;
+    }
+
+}
